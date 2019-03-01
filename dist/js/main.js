b5bca790f029d4b5790419c25c4caf8f7addc30e
Aleksic Dusan <adusan53@gmail.com>
2019-03-01 14:16:31 +0100

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,7 +30,7 @@ function toggleMenu() {
 		menu.classList.remove('show');	
 		menuNav.classList.remove('show');	
 		menuBranding.classList.remove('show');	
-		navItem.forEach(item => item.classList.remove('show'));
+		navItems.forEach(item => item.classList.remove('show'));
 
 
 		//Set Menu state
